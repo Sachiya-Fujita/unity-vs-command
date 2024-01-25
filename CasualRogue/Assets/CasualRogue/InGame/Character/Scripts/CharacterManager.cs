91e015e1b90f4bf6dbdc9628bbaace558b31889d
s4ch1mos <s4ch1mos@gmail.com>
2024-01-25 12:37:53 +0900

    temp

---
+++ CasualRogue/Assets/CasualRogue/InGame/Character/Scripts/CharacterManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CasualRogue
+{
+    /// <summary>
+    /// CharacterManagerのクラス
+    /// </summary>
+    public class CharacterManager : SharedMonoBehaviour<CharacterManager>
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
